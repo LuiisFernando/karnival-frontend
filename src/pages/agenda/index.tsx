6d28991762da3784ed05b6e14837c5cb3a7edeba
luis <lsh.fernandors@gmail.com>
2023-06-29 19:38:10 -0300

    add calendar events

--- src/pages/agenda/index.tsx
+++ src/pages/agenda/index.tsx
@@ -6,10 +6,56 @@ import 'moment/locale/pt-br';
 import moment from 'moment';
 
 moment.locale("moment/locale/pt-br");
+
 const localizer = momentLocalizer(moment);
 
+const HeaderCellContent: React.FC<any> = (props: any) => {
+  const { date } = props;
+  
+  const dayOfWeek = date.getDay();
+
+  const className = dayOfWeek === 0 || dayOfWeek === 6 ? 'classes.day_weekend' : 'classes.day_working';
+  return (
+    <span className={className}>
+      {props.label}
+    </span>
+  );
+};
+
+const DateCellContent: React.FC<any> = (props: any) => {
+  const { date } = props;
+  const dayOfWeek = date.getDay();
+
+  const className = dayOfWeek === 0 || dayOfWeek === 6 ? 'classes.day_weekend' : 'classes.day_working';
+
+    return (
+    <span className={className}>
+      {props.label}
+    </span>
+  );
+};
+
 export default function Agenda() {
-  const [eventsData, setEventsData] = useState<any>([{ start: new Date(), end: new Date(), title: "special event" }]);
+
+  // let dataInicial = moment(new Date("2023-06-31"));
+  // let datafim = moment(new Date("2023-06-31"));
+
+  // console.log(dataInicial);
+
+  
+  const dataInicial1 = new Date(2023, 5, 29, 13, 0);
+  const dataFim1 = new Date(2023, 5, 29, 13, 30);
+
+  const dataInicial12 = new Date(2023, 5, 29, 13, 30);
+  const dataFim2 = new Date(2023, 5, 29, 14, 0, 0);
+
+
+  const eventos = [
+    { start: dataInicial1, end: dataFim1, title: "evento 1", teste: '123' },
+    { start: dataInicial12, end: dataFim2, title: "evento 2", teste: '321' }
+  ]
+
+  const [eventsData, setEventsData] = useState<any>(eventos);
 
   var defaultMessages = {
     date: 'Data',
@@ -33,9 +79,7 @@ export default function Agenda() {
   };
 
   const handleSelect = ({ start, end }: any) => {
-    console.log(start);
-    console.log(end);
-    const title = window.prompt("New Event name");
+    const title = window.prompt("New Event name " + start);
     if (title)
       setEventsData([
         ...eventsData,
@@ -47,23 +91,65 @@ export default function Agenda() {
       ]);
   };
 
+  function eventPropGetter(event: any, start: any, end: any, isSelected: any) {
+    // console.log('evento >> ', event);
+    // var backgroundColor = '#' + event.hexColor;
+    var style = {
+        backgroundColor: 'red',
+        borderRadius: '5px',
+        // opacity: 0.8,
+        color: '#FFF',
+        border: '0px',
+    };
+    return {
+        style: style
+    };
+  }
+
+  const customDayPropGetter = (date: Date) => {
+    if (date.getDate() === 7 || date.getDate() === 6)
+      return {
+        className: 'special-day',
+        style: {
+          border: 'solid 3px ' + (date.getDate() === 7 ? '#faa' : '#afa'),
+          backgroundColor: 'red'
+        },
+      };
+    else return {};
+  };
+
   return (
     <>
       <h1>Agenda</h1>
       <div style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
         <Calendar
+          popup
+          min={new Date(0, 0, 0, 9, 0, 0)}
+          max={new Date(0, 0, 0, 20, 0, 0)}
+          step={30}
           views={["day", "week", "month", "agenda"]}
           culture="pt-BR"
           selectable
           localizer={localizer}
           defaultDate={new Date()}
-          defaultView="month"
+          defaultView="day"
           events={eventsData}
-          style={{ height: '50vh', width: '50%' }}
-          onSelectEvent={(event) => alert(event.title)}
-          onSelectSlot={handleSelect}
+          style={{ height: '600px', width: '90%' }}
+          onSelectEvent={(event) => alert(event.teste)}
+          onSelectSlot={(e) => {
+            console.log(e);
+            handleSelect(e);
+          }}
           messages={defaultMessages}
+          eventPropGetter={eventPropGetter}
+          components={{
+            month: {
+              header: HeaderCellContent,
+              dateHeader: DateCellContent
+            }
+          }}
+          dayPropGetter={customDayPropGetter}
         />
       </div>
     </>
